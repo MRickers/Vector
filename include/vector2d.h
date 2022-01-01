bcee3bcb62c721ab8eb6856fa38da95be0899c21
Max <max_rickers@hotmail.de>
2022-01-01 12:02:00 +0100

    Added empty constructor

--- include/vector2d.h
+++ include/vector2d.h
@@ -4,6 +4,7 @@
 namespace vector {
     class Vector2D {
     public:
+        Vector2D();
         Vector2D(float x, float y);
         Vector2D(const Vector2D& other);
 
--- src/vector2d.cpp
+++ src/vector2d.cpp
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <stdexcept>
 
+vector::Vector2D::Vector2D() : m_x(0), m_y(0) {}
+
 vector::Vector2D::Vector2D(float x, float y) :
 	m_x(x),
 	m_y(y){
